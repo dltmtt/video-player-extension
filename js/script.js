0dcdec6248d9caf5a8192b9a8d5f09ab1a8590df
MDXZ-delti <deltonmatteo@gmail.com>
2022-05-21 11:17:09 +0200

    Don't blur on focus (that was stupid)

--- js/script.js
+++ js/script.js
@@ -155,9 +155,6 @@ video.onended = () => { localStorage.removeItem(videoId) }
 
 // KEYBOARD SHORTCUTS
 document.addEventListener('keydown', (event) => {
-    if (event.target.tagName === 'BUTTON')
-        event.target.blur()
-
     if (event.shiftKey) {
         modPressedDuringKeydown = true
         replayBtn.textContent = 'replay_30'

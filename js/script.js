b0f07e2aa1f58134d2bc292901671f77d3e29e62
MDXZ-delti <deltonmatteo@gmail.com>
2023-02-27 10:40:06 +0100

    Code refactor
    Use arrow functions, add semicolons, improve names and comments

--- js/script.js
+++ js/script.js
@@ -1,73 +1,57 @@
-'use strict'
+'use strict';
 
-var preferences = {
+let preferences = {
 	speed: 1.8,
 	timeSkip: 10
-}
-
-
-// CLEANUP
-// -------
-
-console.groupCollapsed('Checking for localStorage entries older than 30 days…')
-for (const key of Object.keys(localStorage)) {
-	const entryDate = new Date(JSON.parse(localStorage.getItem(key)).timestamp)
-	if (entryDate < new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)) {
-		localStorage.removeItem(key)
-		console.info(`${key} deleted.`)
-	} else {
-		console.info(`${key} kept.`)
-	}
-}
-console.groupEnd()
+};
 
 
 // VIDEO SELECTION
 // ---------------
 
 // DRAG AND DROP
-const dragPanel = document.querySelector('#drag-panel')
-const dropOverlay = document.querySelector('#drop-overlay')
-const droppableElements = document.querySelectorAll('.droppable')
-const fileName = document.querySelector('#file-name')
-const video = document.querySelector('video')
-var localStorageKey
+const dragPanel = document.querySelector('#drag-panel');
+const dropOverlay = document.querySelector('#drop-overlay');
+const droppableElements = document.querySelectorAll('.droppable');
+const fileName = document.querySelector('#file-name');
+const video = document.querySelector('video');
+let localStorageKey;
 
 droppableElements.forEach(droppable => {
-	droppable.addEventListener('dragenter', function (e) {
+	droppable.addEventListener('dragenter', e => {
 		if (e.dataTransfer.items[0].type.startsWith('video/')) {
-			console.info(`A video file has entered #${e.target.id}'s dragging area. Showing the drop overlay…`)
-			this.dataset.fileHover = true
-			dropOverlay.hidden = false
+			droppable.dataset.fileHover = true;
+			dropOverlay.hidden = false;
+			console.info(`A video file has entered #${e.target.id}'s dragging area. The drop overlay is now visible.`);
 		}
-	})
-})
+	});
+});
 
-dropOverlay.addEventListener('dragover', e => e.preventDefault())
+dropOverlay.addEventListener('dragover', e => e.preventDefault());
 
 dropOverlay.addEventListener('drop', async (e) => {
-	console.info(`A ${e.dataTransfer.items[0].type} file was dropped on #${e.target.id}.`)
-	e.preventDefault()
+	console.info(`A ${e.dataTransfer.items[0].type} file was dropped on #${e.target.id}.`);
+	e.preventDefault();
 
 	// Type check is done in dragenter and in the click handler
-	const fileHandle = await e.dataTransfer.items[0].getAsFileSystemHandle()
+	const fileHandle = await e.dataTransfer.items[0].getAsFileSystemHandle();
 
-	manageFileHandle(fileHandle)
-	handleDragEnd()
-})
+	manageFileHandle(fileHandle);
+	handleDragEnd();
+});
 
-dropOverlay.addEventListener('dragleave', handleDragEnd)
+dropOverlay.addEventListener('dragleave', handleDragEnd);
 
 function handleDragEnd() {
-	console.info('The drag event has ended. Hiding the drop overlay…')
-	dropOverlay.hidden = true
+	dropOverlay.hidden = true;
 	droppableElements.forEach(droppable => {
-		delete droppable.dataset.fileHover
-	})
+		delete droppable.dataset.fileHover;
+	});
+	console.info('The drag event has ended. The drop overlay was hidden.');
 }
 
 // FILE INPUT
-const filePicker = document.querySelector('#file-picker')
+const filePicker = document.querySelector('#file-picker');
 filePicker.addEventListener('click', async () => {
 	try {
 		const [fileHandle] = await window.showOpenFilePicker({
@@ -81,54 +65,53 @@ filePicker.addEventListener('click', async () => {
 				}
 			],
 			multiple: false
-		})
+		});
 
-		manageFileHandle(fileHandle)
+		manageFileHandle(fileHandle);
 	} catch (abortError) { }
-})
+});
 
 // FILE HANDLING
 async function manageFileHandle(fileHandle) {
-	const file = await fileHandle.getFile()
+	const file = await fileHandle.getFile();
 
 	if (video.src) {
-		console.info('A video change was detected. Saving the old video state in local storage…')
-		updateLocalStorage()
-		URL.revokeObjectURL(video.src)
+		console.info('A video change was detected. Saving the old video state in local storage…');
+		updateLocalStorage();
+		URL.revokeObjectURL(video.src);
 	} else {
-		console.info('Hiding the drag panel and showing the player…')
-		dragPanel.hidden = true
-		player.hidden = false
+		dragPanel.hidden = true;
+		player.hidden = false;
+		console.info('The drag panel was hidden. The player is now visible.');
 	}
 
 	// Don't change the order of these lines!
-	localStorageKey = await hashFile(file)
-	video.src = URL.createObjectURL(file)
+	localStorageKey = await hashFile(file);
+	video.src = URL.createObjectURL(file);
 
 	// Remove the file extension
-	fileName.textContent = file.name.replace(/\.[^.]+$/, '')
+	fileName.textContent = file.name.replace(/\.[^.]+$/, '');
 
 	// Update the media session on first play
-	video.addEventListener('seeked', function () {
-		console.info('First seek detected. Updating Global Media Controls…')
-		const artwork = capture()
+	video.addEventListener('seeked', () => {
+		const artwork = capture();
 		navigator.mediaSession.metadata = new MediaMetadata({
 			title: fileName.textContent,
 			artwork: [
 				{ src: artwork, sizes: '512x512', type: 'image/png' }
 			]
-		})
-		console.info('Title and artwork for Global Media Controls updated.')
-	}, { once: true })
+		});
+		console.info('Title and artwork for Global Media Controls updated.');
+	}, { once: true });
 
 	// Bind the global media controls to the video
 	const actionHandlers = [
 		['seekbackward', replay],
 		['seekforward', forward]
-	]
+	];
 
 	for (const [action, handler] of actionHandlers) {
-		navigator.mediaSession.setActionHandler(action, handler)
+		navigator.mediaSession.setActionHandler(action, handler);
 	}
 }
 
@@ -137,132 +120,143 @@ async function manageFileHandle(fileHandle) {
 // ----------------
 
 // NAVIGATION
-const player = document.querySelector('.player')
-const playBtn = document.querySelector('.play-btn')
-const fullscreenBtn = document.querySelector('.fullscreen-btn')
-const zoomBtn = document.querySelector('.zoom-btn')
-const speedControls = document.querySelector('#speed-controls')
+const player = document.querySelector('.player');
+const playBtn = document.querySelector('.play-btn');
+const fullscreenBtn = document.querySelector('.fullscreen-btn');
+const zoomBtn = document.querySelector('.zoom-btn');
+const speedControls = document.querySelector('#speed-controls');
 
 // Play/pause
-playBtn.onclick = togglePlay
-video.onclick = togglePlay
-video.onpause = () => { playBtn.textContent = 'play_arrow' }
-video.onplay = () => { playBtn.textContent = 'pause' }
+playBtn.onclick = togglePlay;
+video.onclick = togglePlay;
+video.onpause = () => { playBtn.textContent = 'play_arrow'; };
+video.onplay = () => { playBtn.textContent = 'pause'; };
 
 // Fullscreen
-fullscreenBtn.onclick = toggleFullScreen
-document.onfullscreenchange = function () {
+fullscreenBtn.onclick = toggleFullScreen;
+document.onfullscreenchange = () => {
 	fullscreenBtn.textContent = (document.fullscreenElement) ?
 		'fullscreen_exit' :
-		'fullscreen'
-}
+		'fullscreen';
+};
 
-video.addEventListener('dblclick', toggleFullScreen)
+video.addEventListener('dblclick', toggleFullScreen);
 
 // Speed
-video.onratechange = function () {
-	speedControls.value = this.playbackRate.toFixed(2)
-}
+video.onratechange = () => {
+	speedControls.value = video.playbackRate.toFixed(2);
+};
 
-speedControls.onchange = function () {
+speedControls.onchange = () => {
 	// Caused by keyboard shortcuts
-	this.value = parseFloat(this.value).toFixed(2)
-	video.playbackRate = clamp(0.1, this.value, 16)
-}
+	speedControls.value = parseFloat(speedControls.value).toFixed(2);
+	video.playbackRate = clamp(0.1, speedControls.value, 16);
+};
 
-speedControls.oninput = function () {
+speedControls.oninput = () => {
 	// Caused by keyboard input
-	this.value = parseFloat(this.value).toFixed(2)
-}
+	speedControls.value = parseFloat(speedControls.value).toFixed(2);
+};
 
 // Zoom
-zoomBtn.onclick = toggleZoom
+zoomBtn.onclick = toggleZoom;
 
 
 // TIME
 // ----
 
-const videoBar = document.querySelector('#video-bar')
-const timeIndicator = document.querySelector('#time-indicator')
-const currentTime = document.querySelector('.current-time')
-const timeRemaining = document.querySelector('.time-remaining')
-const replayBtn = document.querySelector('.replay-btn')
-const forwardBtn = document.querySelector('.forward-btn')
-const duration = document.querySelector('.duration')
+const videoBar = document.querySelector('#video-bar');
+const timeIndicator = document.querySelector('#time-indicator');
+const currentTime = document.querySelector('.current-time');
+const timeRemaining = document.querySelector('.time-remaining');
+const replayBtn = document.querySelector('.replay-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const duration = document.querySelector('.duration');
 
-video.addEventListener('loadedmetadata', function () {
+video.addEventListener('loadedmetadata', () => {
 	if (localStorage.getItem(localStorageKey)) {
-		console.info('Video state found in local storage. Restoring…')
-		restoreFromLocalStorage()
+		restoreFromLocalStorage();
 	} else {
-		console.info('No video state found in local storage.')
+		console.info('No video state found in local storage.');
 	}
 
-	updateTimeIndicators()
-	duration.textContent = secondsToTime(this.duration)
+	updateTimeIndicators();
+	duration.textContent = secondsToTime(video.duration);
 
-	videoBar.setAttribute('max', this.duration)
-	updateVideoBar()
-})
+	videoBar.setAttribute('max', video.duration);
+	updateVideoBar();
+});
 
-video.addEventListener('emptied', function () {
+video.addEventListener('emptied', () => {
 	// Needed when another video is loaded while the current one is playing
-	playBtn.textContent = 'play_arrow'
-})
+	playBtn.textContent = 'play_arrow';
+});
 
-video.addEventListener('timeupdate', function () {
+video.addEventListener('timeupdate', () => {
 	if (video.readyState < HTMLMediaElement.HAVE_METADATA) {
-		console.info('The video metadata is not loaded yet. Skipping timeupdate event.')
-		return
+		console.info('The video metadata is not loaded yet. Skipping timeupdate event.');
+		return;
 	}
 
-	updateVideoBar()
-	updateTimeIndicators()
-})
+	updateVideoBar();
+	updateTimeIndicators();
+});
 
 // Seek to the point clicked on the progress bar
-videoBar.addEventListener('input', function () {
-	videoBar.style.setProperty("--progress", (this.valueAsNumber * 100 / video.duration) + "%")
+videoBar.addEventListener('input', () => {
+	videoBar.style.setProperty("--progress", (videoBar.valueAsNumber * 100 / video.duration) + "%");
 
-	video.currentTime = this.value
+	video.currentTime = videoBar.value;
 
-	// Needed to show live the time when the progress bar is dragged
-	updateTimeIndicators()
-})
+	// Needed to show the time in real-time when the progress bar is dragged
+	updateTimeIndicators();
+});
 
 function updateTimeIndicators() {
-	currentTime.textContent = secondsToTime(video.currentTime)
-	timeRemaining.textContent = `-${secondsToTime(video.duration - video.currentTime)}`
+	currentTime.textContent = secondsToTime(video.currentTime);
+	timeRemaining.textContent = `-${secondsToTime(video.duration - video.currentTime)}`;
 }
 
 function updateVideoBar() {
-	videoBar.value = video.currentTime
-	videoBar.style.setProperty("--progress", (videoBar.valueAsNumber * 100 / video.duration) + "%")
+	videoBar.value = video.currentTime;
+	videoBar.style.setProperty("--progress", (videoBar.valueAsNumber * 100 / video.duration) + "%");
 }
 
 // videoBar also has tabindex="-1"
-videoBar.onfocus = function () { this.blur() }
+videoBar.onfocus = () => { videoBar.blur(); };
 
-replayBtn.onclick = replay
-forwardBtn.onclick = forward
+replayBtn.onclick = replay;
+forwardBtn.onclick = forward;
 
 // Toggle current time/remaining time
-timeIndicator.addEventListener('click', function () {
-	[timeRemaining.hidden, currentTime.hidden] = [currentTime.hidden, timeRemaining.hidden]
-})
+timeIndicator.addEventListener('click', () => {
+	[timeRemaining.hidden, currentTime.hidden] = [currentTime.hidden, timeRemaining.hidden];
+});
 
 // Save time in local storage when the window is closed/refreshed
 window.onbeforeunload = () => {
 	if (video.src && !video.ended) {
-		updateLocalStorage()
+		updateLocalStorage();
+	}
+};
+
+// CLEANUP
+console.groupCollapsed('Saved states of videos last opened more than 30 days ago will be deleted.');
+for (const key of Object.keys(localStorage)) {
+	const entryDate = new Date(JSON.parse(localStorage.getItem(key)).last_opened);
+	if (entryDate < new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)) {
+		localStorage.removeItem(key);
+		console.info(`${key} deleted.`);
+	} else {
+		console.info(`${key} kept.`);
 	}
 }
+console.groupEnd();
 
-// Delete video state from local storage
 video.onended = () => {
-	console.info('Video ended. Deleting video state from local storage…')
-	localStorage.removeItem(localStorageKey)
-}
+	localStorage.removeItem(localStorageKey);
+	console.info('Video ended. Video state deleted from local storage.');
+};
 
 
 // KEYBOARD SHORTCUTS
@@ -271,78 +265,78 @@ video.onended = () => {
 document.addEventListener('keydown', (e) => {
 	// Ignore key presses when a modifier key is pressed
 	if (e.ctrlKey || e.altKey || e.metaKey || e.shiftKey)
-		return
+		return;
 
 	if (e.key !== ' ') {
-		document.activeElement.blur()
+		document.activeElement.blur();
 	}
 
 	switch (e.key) {
 		case ' ': // Toggle play
-			if (document.activeElement.tagName == 'BUTTON')
-				break
+			if (document.activeElement.tagName === 'BUTTON')
+				break;
 		case 'k':
-			togglePlay()
-			break
+			togglePlay();
+			break;
 		case 's': // Slow down
 		case 'S':
-			speedControls.stepDown()
-			speedControls.dispatchEvent(new Event('change'))
-			break
+			speedControls.stepDown();
+			speedControls.dispatchEvent(new Event('change'));
+			break;
 		case 'd': // Speed up
 		case 'D':
-			speedControls.stepUp()
-			speedControls.dispatchEvent(new Event('change'))
-			break
+			speedControls.stepUp();
+			speedControls.dispatchEvent(new Event('change'));
+			break;
 		case 'z': // Rewind
 		case 'Z':
 		case 'ArrowLeft':
 		case 'ArrowDown':
 			if (document.activeElement.tagName !== 'INPUT')
-				replay()
-			break
+				replay();
+			break;
 		case 'x': // Advance
 		case 'X':
 		case 'ArrowRight':
 		case 'ArrowUp':
 			if (document.activeElement.tagName !== 'INPUT')
-				forward()
-			break
+				forward();
+			break;
 		case 'r': // Reset speed
-			video.playbackRate = video.defaultPlaybackRate
-			break
+			video.playbackRate = video.defaultPlaybackRate;
+			break;
 		case 't': // Toggle time indicator
-			toggleTimeIndicator()
-			break
+			toggleTimeIndicator();
+			break;
 		case 'a': // Preferred fast speed
-			video.playbackRate = preferences.speed
-			break
+			video.playbackRate = preferences.speed;
+			break;
 		case 'm': // Toggle mute
-			toggleMute()
-			break
+			toggleMute();
+			break;
 		case 'c': // Toggle zoom
-			toggleZoom()
-			break
+			toggleZoom();
+			break;
 		case 'p': // Toggle PiP
-			togglePictureInPicture()
-			break
+			togglePictureInPicture();
+			break;
 		case 'f':
 		case 'Enter':
 			if (document.activeElement.tagName !== 'BUTTON' && document.activeElement.tagName !== 'INPUT')
-				toggleFullScreen()
+				toggleFullScreen();
 	}
-})
+});
 
 function togglePlay() {
-	video.paused ? video.play() : video.pause()
+	video.paused ? video.play() : video.pause();
 }
 
 function toggleMute() {
-	video.muted = !video.muted
+	video.muted = !video.muted;
 }
 
 function clamp(min, value, max) {
-	return Math.min(Math.max(value, min), max)
+	return Math.min(Math.max(value, min), max);
 }
 
 function replay() {
@@ -356,33 +350,33 @@ function forward() {
 function togglePictureInPicture() {
 	(document.pictureInPictureElement) ?
 		document.exitPictureInPicture() :
-		video.requestPictureInPicture()
+		video.requestPictureInPicture();
 }
 
 function toggleFullScreen() {
 	(document.fullscreenElement) ?
 		document.exitFullscreen() :
-		player.requestFullscreen()
+		player.requestFullscreen();
 }
 
 function toggleZoom() {
 	if (zoomBtn.textContent === 'zoom_out_map') {
-		video.style.objectFit = 'cover'
-		zoomBtn.textContent = 'crop_free'
+		video.style.objectFit = 'cover';
+		zoomBtn.textContent = 'crop_free';
 	} else {
-		video.style.objectFit = 'contain'
-		zoomBtn.textContent = 'zoom_out_map'
+		video.style.objectFit = 'contain';
+		zoomBtn.textContent = 'zoom_out_map';
 	}
 }
 
 function toggleTimeIndicator() {
-	[currentTime.hidden, timeRemaining.hidden] = [timeRemaining.hidden, currentTime.hidden]
+	[currentTime.hidden, timeRemaining.hidden] = [timeRemaining.hidden, currentTime.hidden];
 }
 
 // Convert seconds to time in format (h:)mm:ss
 // Use https://tc39.es/proposal-temporal/docs/duration.html when available
 function secondsToTime(seconds) {
-	return new Date(seconds * 1000).toISOString().substring((seconds >= 3600) ? 12 : 14, 19)
+	return new Date(seconds * 1000).toISOString().substring((seconds >= 3600) ? 12 : 14, 19);
 }
 
 
@@ -397,10 +391,11 @@ async function hashFile(file) {
 	const hashAsArrayBuffer = await crypto.subtle.digest('SHA-1', arrayBuffer);
 
 	// Get the hex value of each byte and store it in an array
-	const uint8ViewOfHash = new Uint8Array(hashAsArrayBuffer);
+	const hashAsUint8 = new Uint8Array(hashAsArrayBuffer);
+	const hashAsArray = Array.from(hashAsUint8);
 
 	// Convert each byte to a hex string
-	const hashAsString = Array.from(uint8ViewOfHash).map((b) => b.toString(16).padStart(2, '0')).join('');
+	const hashAsString = hashAsArray.map(b => b.toString(16).padStart(2, '0')).join('');
 	return hashAsString;
 }
 
@@ -408,32 +403,31 @@ function updateLocalStorage() {
 	let state = {
 		timer: video.currentTime,
 		playbackRate: video.playbackRate,
-		timestamp: Date.now()
-	}
-	localStorage.setItem(localStorageKey, JSON.stringify(state))
-	console.info('Video state saved in local storage.')
+		last_opened: Date.now()
+	};
+	localStorage.setItem(localStorageKey, JSON.stringify(state));
+	console.info('Video state saved in local storage.');
 }
 
 function restoreFromLocalStorage() {
-	let state = JSON.parse(localStorage.getItem(localStorageKey))
-	video.currentTime = state.timer
-	video.playbackRate = state.playbackRate
-	console.info('Video state restored from local storage.')
+	let state = JSON.parse(localStorage.getItem(localStorageKey));
+	video.currentTime = state.timer;
+	video.playbackRate = state.playbackRate;
+	console.info('Video state restored from local storage.');
 }
 
 function capture() {
-	console.info('Capturing a screenshot of the video…')
-	const canvas = document.createElement('canvas')
-	canvas.width = canvas.height = 512
+	const canvas = document.createElement('canvas');
+	canvas.width = canvas.height = 512;
 
-	const scale = Math.min(canvas.width / video.videoWidth, canvas.height / video.videoHeight)
-	const x = (canvas.width / 2) - (video.videoWidth / 2) * scale
-	const y = (canvas.height / 2) - (video.videoHeight / 2) * scale
+	const scale = Math.min(canvas.width / video.videoWidth, canvas.height / video.videoHeight);
+	const x = (canvas.width / 2) - (video.videoWidth / 2) * scale;
+	const y = (canvas.height / 2) - (video.videoHeight / 2) * scale;
 
-	const ctx = canvas.getContext('2d')
-	ctx.drawImage(video, x, y, video.videoWidth * scale, video.videoHeight * scale)
+	const context = canvas.getContext('2d');
+	context.drawImage(video, x, y, video.videoWidth * scale, video.videoHeight * scale);
 
-	const dataURL = canvas.toDataURL()
+	const dataURL = canvas.toDataURL();
 
-	return dataURL
+	return dataURL;
 }
